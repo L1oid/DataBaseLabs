f003a4d0faf19391182a0c296d0ba3e8d6e9fa0a
L1oid <lloidtm@gmail.com>
2022-12-05 11:16:14 +0700

    upd

--- Lab8/exampleConnect.js
+++ Lab8/exampleConnect.js
@@ -6,7 +6,30 @@ async function run() {
 		await mongoClient.connect();
 		const db = mongoClient.db("FirstBase");
 		const collection = db.collection("ScienceJournal");
-		await collection.insertOne({test: "testDocument"});
+		await collection.insertOne({
+			title: "Статья 1",
+			authors: ["Иванов Иван"],
+			date: new Date("03.12.2022"),
+			content: "Текст статьи 1",
+			tags: ["статья 1", "программирование", "веб", "инженерия"],
+			reviews: [
+				{
+					name: "Алексей",
+					comment: "Лучшая статья !!",
+					mark: 10,
+				},
+				{
+					name: "Максим",
+					comment: "Я научился программировать!!!",
+					mark: 9,
+				},
+				{
+					name: "Дмитрий",
+					comment: "Ничего не понятно((",
+					mark: 2,
+				}
+			]
+		});
 		await collection.deleteMany({});
 	} catch (err) {
 		console.log(err);
